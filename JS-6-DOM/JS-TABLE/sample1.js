dfb9f0cbe7d099299c730b0b4d105e5db82e7f1d
AKALYASRIJAK <akalyaksreejak@gmail.com>
2022-04-08 19:24:32 +0530

    today code

---
+++ JS-6-DOM/JS-TABLE/sample1.js
@@ -0,0 +1,49 @@
+food=[{
+    coffeetypes:"cappuccino",
+    teatypes:"green tea",
+    snackstypes:"popcorn",
+    juicetypes:"apple juice",
+    fastfood:"hamburger", 
+},
+{
+    coffeetypes:"latte",
+    teatypes:"black tea",
+    snackstypes:"trail mix",
+    juicetypes:"blueberry juice",
+    fastfood:"cheeseburger",
+   
+},
+{
+    coffeetypes:"americano",
+    teatypes:"oolong tea",
+    snackstypes:"potato chips",
+   juicetypes:"beet juice",
+    fastfood:"sandwich",
+   
+},
+{
+    coffeetypes:"espresso",
+    teatypes:"assam tea",
+    snackstypes:"yogurt",
+    juicetypes:"grape juice",
+    fastfood:"muffin",
+    
+},
+{
+    coffeetypes:"iced coffee",
+    teatypes:"herbal tea",
+    snackstypes:"jerky",
+    fastfood:"taco",
+    juicetypes:"cranberry juice",
+},
+
+
+
+
+
+
+
+
+
+
+]
