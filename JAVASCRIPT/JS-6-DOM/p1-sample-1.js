63a22080273689e15e4785830b21d724175dbb54
srividya-T <vidyachowdary7@gmail.com>
2022-04-08 21:12:40 +0530

    p-samples

---
+++ JAVASCRIPT/JS-6-DOM/p1-sample-1.js
@@ -0,0 +1,36 @@
+var students = [
+    {
+        name: "Vidya",
+        age: 31,
+        mobile: 9398795273,
+        city: "Hyderabad",
+        state: "Telangana",
+        pincode: 500090,
+      },
+      {
+        name: "Vasu",
+        age: 37,
+        mobile: 9398795273,
+        city: "Hyderabad",
+        state: "Telangana",
+        pincode: 500090,
+      },
+      
+      {
+        name: "Srividya",
+        age: 31,
+        mobile: 9398795273,
+        city: "Hyderabad",
+        state: "Telangana",
+        pincode: 500090,
+      },
+      
+      {
+        name: "sriniasu",
+        age: 37,
+        mobile: 9398795273,
+        city: "Hyderabad",
+        state: "Telangana",
+        pincode: 500090,
+      },
+]
