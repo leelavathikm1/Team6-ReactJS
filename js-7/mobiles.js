5f5ec032835a6756d7a8af63de9e195e8801acc5
deepthiboppana <harideepthiboppana@gmail.com>
2022-04-09 09:09:12 +0530

    tables and looping

---
+++ js-7/mobiles.js
@@ -0,0 +1,50 @@
+var mobiles=[
+    {
+        modelName:"Redmi note 11 pro",
+        brand:"Redmi",
+        memory:"12gb",
+        formFactor:"Bar",
+        os:"MUI 13",
+        model:2022
+    },
+    {
+        modelName:"samsung m13",
+        brand:"samsung",
+        memory:"12gb",
+        formFactor:"Bar",
+        os:"MUI 13",
+        model:2022
+    },
+    {
+        modelName:"iphone 6s",
+        brand:"apple",
+        memory:"12gb",
+        formFactor:"Bar",
+        os:"mac",
+        model:2022
+    },
+    {
+        modelName:"nokia pro",
+        brand:"nokia",
+        memory:"12gb",
+        formFactor:"Bar",
+        os:"MUI 13",
+        model:2022
+    },
+    {
+        modelName:"oppo pro",
+        brand:"oppo",
+        memory:"12gb",
+        formFactor:"Bar",
+        os:"MUI 13",
+        model:2022
+    },
+    {
+        modelName:"oneplus 8T",
+        brand:"oneplus",
+        memory:"12gb",
+        formFactor:"Bar",
+        os:"MUI 13",
+        model:2022
+    },
+];
