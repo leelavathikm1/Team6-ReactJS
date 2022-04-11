6b67575e7ab6854f08e6427dfd5fd17a79a6b89f
JayasankarSaiKiran <adhi88640@gmail.com>
2022-04-11 14:09:50 +0530

    practices

---
+++ JAVASCRIPT/task-1/table1.js
@@ -0,0 +1,92 @@
+var details=[
+    {
+        "fname": "Geraldine",
+        "lname": "Bélanger",
+        "tel": "(820)905-5342",
+        "address": "7209 Nunc Ln",
+        "city": "Durham",
+        "state": "MS",
+        "zip": 66304
+    },
+    {
+        "fname": "Terry",
+        "lname": "Stalinski",
+        "tel": "(309)067-4643",
+        "address": "398 Elit Ln",
+        "city": "Circleville",
+        "state": "ID",
+        "zip": 28256
+    },
+    {
+        "fname": "Loknath",
+        "lname": "Christopher",
+        "tel": "(793)102-9251",
+        "address": "8031 Augue Ave",
+        "city": "Winfield",
+        "state": "GA",
+        "zip": 35520
+    },
+    {
+        "fname": "Cenna",
+        "lname": "Snyder",
+        "tel": "(772)889-2364",
+        "address": "6818 Eros Ln",
+        "city": "Litchfield Park",
+        "state": "TX",
+        "zip": 31216
+    },
+    {
+        "fname": "Fiaze",
+        "lname": "Spiegel",
+        "tel": "(451)113-3866",
+        "address": "3750 Risus Ln",
+        "city": "Chester Springs",
+        "state": "SD",
+        "zip": 48750
+    },
+    {
+        "fname": "Ricardo",
+        "lname": "Marina",
+        "tel": "(867)936-1340",
+        "address": "8373 Ipsum Ln",
+        "city": "Silicon Valley",
+        "state": "VT",
+        "zip": 11656
+    },
+    {
+        "fname": "Kerri",
+        "lname": "Frame",
+        "tel": "(240)975-9289",
+        "address": "8612 Ipsum Ct",
+        "city": "Oxford",
+        "state": "OR",
+        "zip": 27295
+    },
+    {
+        "fname": "Hae-Yong",
+        "lname": "Dibernardo",
+        "tel": "(170)409-6444",
+        "address": "4557 Tellus Dr",
+        "city": "Cedartown",
+        "state": "IA",
+        "zip": 53976
+    },
+    {
+        "fname": "Guinka",
+        "lname": "Herbert",
+        "tel": "(104)251-8638",
+        "address": "2712 Pulvinar Rd",
+        "city": "Saltilo",
+        "state": "NE",
+        "zip": 83544
+    },
+    {
+        "fname": "Rodney",
+        "lname": "Gerela",
+        "tel": "(956)920-1184",
+        "address": "2130 Dolor St",
+        "city": "Killeen",
+        "state": "HI",
+        "zip": 84733
+    }
+]
